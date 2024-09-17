e1eec107eccc4fd3d353e6c925637b7cec6f650c
Nguyen Hau <nguyenhau.sbm@gmail.com>
2024-09-17 14:49:33 -0500

    test

---
+++ firebase_config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7ebVoo4cI5zOcAekGzAzHxuWMpv0iMAo",
+  authDomain: "girl-scout-cookies-app.firebaseapp.com",
+  projectId: "girl-scout-cookies-app",
+  storageBucket: "girl-scout-cookies-app.appspot.com",
+  messagingSenderId: "1064101303313",
+  appId: "1:1064101303313:web:5f09c397328124f9da4db0",
+  measurementId: "G-Q2629JEKY4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
