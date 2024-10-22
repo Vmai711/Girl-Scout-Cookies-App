8edfdea0fc8ed1467d733e2a354fef612b681fed
Vince Mai <151262094+Vmai711@users.noreply.github.com>
2024-10-22 14:02:52 -0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import firebase from './firebase/firebase';
-import { auth } from "./firebase/firebase"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
