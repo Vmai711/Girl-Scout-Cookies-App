32b0e3a317a05a5e98509a6ad1a17eb4050d0fc0
Nguyen Hau <nguyenhau.sbm@gmail.com>
2025-01-10 20:38:40 -0600

    HTML to React pages

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyB7ebVoo4cI5zOcAekGzAzHxuWMpv0iMAo",
   authDomain: "girl-scout-cookies-app.firebaseapp.com",
+  databaseURL: "https://girl-scout-cookies-app-default-rtdb.firebaseio.com",
   projectId: "girl-scout-cookies-app",
   storageBucket: "girl-scout-cookies-app.appspot.com",
   messagingSenderId: "1064101303313",
@@ -18,10 +14,10 @@ const firebaseConfig = {
   measurementId: "G-Q2629JEKY4"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 
-export {app, auth, db};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+
+export { app, auth, db };
