6cbb1691b9ab37060789d576b711f7720d2f2121
JohanyF <johanyf627@gmail.com>
2025-02-09 17:34:14 -0600

    Added custom colors: Dark Green and Light Gray based on our Figma design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js,jsx}'
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-dark-green": "#00C558",
+        "custom-light-gray": "#FAFAFA",
+      },
+    },
   },
   plugins: [],
-}
-
+};
