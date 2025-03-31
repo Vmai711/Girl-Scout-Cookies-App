b2e431e0ae516d6d8801892222ab30597b73b575
Vmai711 <vmai71102@gmail.com>
2025-03-31 04:41:53 -0500

    Updated booth summary UI

--- src/components/summary/boothsummary.jsx
+++ src/components/summary/boothsummary.jsx
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom";
 
+import Header from "../header";
+import SideBar from "../sidebar/sidebar";
+
 const BoothSummary = () => {
     const location = useLocation();
     const storedData = localStorage.getItem("boothData");
@@ -9,18 +12,45 @@ const BoothSummary = () => {
     if (!boothData) {
         return <p>Error: No reservation data found.</p>;
     }
-    
+
     return (
-        <div>
-            <h1>Reservation Summary</h1>
-            <p>Email: {boothData.email}</p>
-            <p>Scout Name: {boothData.girlName}</p>
-            <p>Parent Name: {boothData.parentName}</p>
-            <p>Location: {boothData.boothLocation}</p>
-            <p>Starting Time: {boothData.startingTime}</p>
-            <p>Date: {boothData.date}</p>
+        <div className="bg-custom-light-gray flex min-h-screen">
+          <SideBar />
+          <div className="w-full h-fit sm:ml-64">
+            <Header page={"Booth Summary"}/>
+            <main className="mt-[3.5rem] p-8 bg-gray-100 min-h-screen">
+              <div className="bg-white p-6 rounded-md shadow-md">
+                <h1 className="text-3xl font-bold mb-6 text-center">Booth Summary</h1>
+                <div className="w-full p-2">
+                    <p>Email: {boothData.email}</p>
+                </div>
+
+                <div className="w-full p-2">
+                    <p>Scout Name: {boothData.girlName}</p>
+                </div>
+
+                <div className="w-full p-2">
+                    <p>Parent Name: {boothData.parentName}</p>
+                </div>
+
+                <div className="w-full p-2">
+                    <p>Location: {boothData.boothLocation}</p>
+                </div>
+
+                <div className="w-full p-2">
+                    <p>Starting Time: {boothData.startingTime}</p>
+                </div>
+
+                <div className="w-full p-2">
+                    <p>Date: {boothData.date}</p>
+                </div> 
+
+              </div>
+            </main>
+          </div>
         </div>
-    );
+      );
+
 };
 
 export default BoothSummary;
